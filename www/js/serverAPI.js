49e1ce4aab03ceef4ac324e08888ae162abc357a
Mats77 <m.wieben@yahoo.de>
2014-12-03 11:57:38 +0100

    Dummy Funktionen für ServerAPI
    Alle besprochenen Methoden sind implementiert, geben aber nur einen
    Fehler zurück

---
+++ www/js/serverAPI.js
@@ -0,0 +1,19 @@
+//Error codes:
+//9999 --> Not implemented
+
+
+function createNewUser(username, password, mail) {
+    return -9999;
+}
+
+function loginWithUsername(username, password) {
+    return -9999;
+}
+
+function loginWithSessionKey(sessionkey, password) {
+    return -9999;
+}
+
+function searchPartnerToPlayWith(long, lat, userId) {
+    return -9999;
+}
