65d557584a716639d1ea9b022198fb7a183f66fa
Christian <c.mahlich@gmail.com>
2014-12-03 19:32:51 +0100

    Clean up and install grunt server

--- platforms/ios/www/scripts/controllers.js
+++ platforms/ios/www/scripts/controllers.js
@@ -1,16 +1,16 @@
 'use strict';
 angular.module('WhoU.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('HomeCtrl', function ($scope) {
+    $scope.username = "Chris"
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('FriendsCtrl', function ($scope, Friends) {
+    $scope.friends = Friends.all();
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+.controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+    $scope.friend = Friends.get($stateParams.friendId);
 })
 
-.controller('AccountCtrl', function($scope) {
-});
+.controller('AccountCtrl', function ($scope) {});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,25 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('HomeCtrl', function ($scope) {
+    $scope.username = "maax",
+    $scope.buttonType = "icon ion-search",
+    $scope.buttonDisable = false,
+    $scope.click = function () {
+        $scope.buttonDisable = true;
+    };
+
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('FriendsCtrl', function ($scope, Friends) {
+    $scope.friends = Friends.all();
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+.controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+    $scope.friend = Friends.get($stateParams.friendId);
 })
 
-.controller('AccountCtrl', function($scope) {
-})
+.controller('AccountCtrl', function ($scope) {})
 
-.controller('PlayCtrl', function($scope) {
+.controller('PlayCtrl', function ($scope) {
     $scope.name = 'Megan';
-});   
-           
+});
