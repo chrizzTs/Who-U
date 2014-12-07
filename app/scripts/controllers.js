5b0dc56a3ac6256a83cbce6e83ff46ede0e47435
Vincentxyz <vincentmzw@gmail.com>
2014-12-07 18:06:11 +0100

    Slider und Berichtigung möglicher Fehler
    Slider funktioniert

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -30,46 +30,6 @@ angular.module('starter.controllers', [])
         {image: 'img/Megan_1.jpg', description: 'Megan 1'},
         {image: 'img/Megan_2.jpg', description: 'Megan 2'}
     ];
-    
-    $scope.currentIndex = 0;
-    
-    $scope.setCurrentSlideIndex = function(index){
-        $scope.currentIndex = index;
-    }
-    
-    $scope.isCurrentSlideIndex = function(index){
-        return $scope.currentIndex === index;
-    }
-    
-        $scope.prevSlide = function () {
-            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-        };
-
-        $scope.nextSlide = function () {
-            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-        };
 })
 
-.animation('.slide-animation', function () {
-        return {
-            addClass: function (element, className, done) {
-                if (className == 'ng-hide') {
-                    TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done });                   
-                }
-                else {
-                    done();
-                }
-            },
-            removeClass: function (element, className, done) {
-                if (className == 'ng-hide') {
-                    element.removeClass('ng-hide');
-
-                    TweenMax.set(element, { left: element.parent().width() });
-                    TweenMax.to(element, 0.5, {left: 0, onComplete: done });
-                }
-                else {
-                    done();
-                }
-            }
-        };
-    });
+;
