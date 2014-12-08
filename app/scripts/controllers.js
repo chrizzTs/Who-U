92531f59cece094c2011cd314e6fff16bd16a7ed
Christian <c.mahlich@gmail.com>
2014-12-08 13:11:23 +0100

    Bugfix play-screen

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', ['home', 'settings', 'chatMaster'])
+angular.module('starter.controllers', ['home', 'play', 'settings', 'chatMaster'])
 
 .animation('.slide-animation', function () {
     return {
