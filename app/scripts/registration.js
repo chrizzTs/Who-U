f52e3c2b173e69eb2e9c20498a1c86c5fd022ca8
Simon-graiff <s.graiff@outlook.com>
2014-12-11 13:49:43 +0100

    login und registration sprint1

--- app/scripts/registration.js
+++ app/scripts/registration.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('registration', ['ServerAPI'])
-    .controller('regCtrl', function ($scope) {
+    .controller('regCtrl', function ($scope, serverAPI) {
 
         $scope.user;
         $scope.password1;
@@ -12,7 +12,7 @@ angular.module('registration', ['ServerAPI'])
         $scope.submit = function () {
             if ($scope.password1 === $scope.password2) {
                 console.log('Formular wurde abgeschickt');
-                createNewUser($scope.user, $scope.password1, $scope.EMail);
+                serverAPI.createNewUser($scope.user, $scope.password1, $scope.EMail);
             } else {
                 console.log('Fehler bei den Passwörtern');
             }
@@ -32,11 +32,14 @@ angular.module('registration', ['ServerAPI'])
         //Currently errors
         $scope.$watch('password2', function () {
             if ($scope.password2 == $scope.password1) {
-                console.log('Sind gleich');
+                if ($scope.password2 == null) {
+                    $scope.showWarningEmpty = true;
+                }
+                $scope.showWarningPW2 = false;
             } else {
                 console.log('Sind ungleich');
-                $scope.showWarningPW2 = $scope.password2 ? true : false;
-                //$scope.showWarningPW2 = $scope.password2 ? false : true;
+                $scope.showWarningEmpty = false;
+                $scope.showWarningPW2 = true;
             }
 
         });
@@ -46,28 +49,3 @@ angular.module('registration', ['ServerAPI'])
         });
 
     });
-
-// Easy way to wait for all videos to load before start playing
-
-var promises = [];
-
-function makePromise(i, video) {
-    promises[i] = new $.Deferred();
-    // This event tells us video can be played all the way through, without stopping or buffering
-    video.oncanplaythrough = function () {
-        // Resolve the promise
-        promises[i].resolve();
-    }
-}
-// Pause all videos and create the promise array
-/*$('video').each(function (index) {
-    this.pause();
-    makePromise(index, this);
-})*/
-
-// Wait for all promises to resolve then start playing
-/*$.when.apply(null, promises).done(function () {
-    $('video').each(function () {
-        this.play();
-    });
-});*/
