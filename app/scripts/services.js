3fee0565df6cc56ffadb7211aa10156300327594
Christian <c.mahlich@gmail.com>
2014-12-15 12:25:00 +0100

    Finish news Board and history stack (Complete Sprint 1)

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,6 +2,30 @@
 var services = angular.module('services', [])
 
 
+//Init the history array
+
+var data2 = {
+    "typ": "message",
+    "from": "Max",
+    "title": "Hallo neue Kontakt"
+};
+
+var data1 = {
+    "typ": "played",
+    "date": "04-21-2014",
+    "person": "Tim Federmann"
+};
+
+var arrayData = {
+    'array': [
+            data1,
+            data2
+                ]
+}
+
+localStorage.setItem('history', JSON.stringify(arrayData));
+
+//End init
 
 
 //Create a hitsotry of Events to see the recent Events
@@ -11,91 +35,23 @@ services.factory('localStorageService', function () {
         addEvent: function (event) {
 
 
-            //Init some Data 
-            var data1 = {
-                "id": "file1",
-                "value": "File1",
-                "popup": "sdds1"
-            }
-            var data2 = {
-                "id": "file1",
-                "value": "File1",
-                "popup": "sdds1"
-            }
-            var history = [JSON.stringify(data1), JSON.stringify(data2)];
-            localStorage.setItem('history', (history.toString()));
-
-            //End init
-
             //Pull the history of local storage
-            var history = localStorage.getItem('history');
-            var historyTmp = [JSON.stringify(event)];
+            var history = JSON.parse(localStorage.getItem('history'));
+            var date = event.date.getDate();
+            var month = event.date.getMonth() + 1;
+            var year = event.date.getFullYear();
 
-            //Update the history, so the newest entry at the beginning.
-            if (history.length > 1) {
-                history = historyTmp + ',' + history;
-            } else {
-                history = historyTmp;
-            }
+            event.date = month.toString() + '-' + date.toString() + '-' + year.toString();
 
+            history.array.unshift(event);
 
-            //Save new history to local storage
-            localStorage.setItem('history', history);
+            localStorage.setItem('history', JSON.stringify(history));
+
         },
 
         getHistory: function () {
-            var history = localStorage.getItem('history').split('},');
-            //Add ending bracket, which was deltedt by the split above to make it suitable to an JSON object again.
-            for (var i = 0; i < history.length; i++) {
-                history[i] = history[i] + '}';
-            };
-            return history;
+            return JSON.parse(localStorage.getItem('history')).array;
         }
 
     }
 });
-
-
-
-/*  var lala = localStorage.getItem('events');
-  if (lala == null) {
-      console.log("null");
-  }
-
-  var i = 3;
-
-
-  var sddsd + i = localStorage.getItem('sdsdsd');
-  if (sddsd == null) {
-      console.log("null")
-  }
-
-   //Pull the history
-  var history = widow.localStorage.getItem('events');
-
-  var neu = {
-      "id": "file2",
-      "value": "File2",
-      "popup": "sdds2"
-  };
-
-  neu
-  var updatedHistory = neu;
-  updatedHistory.push()*/
-
-
-
-/*overwrite the localStorage with the newest Event added in 
-                the begining */
-//            var eventUpdatet = newEvent;
-//            eventUpdatet.push(events);
-//            localStorage.setItem('events', JSON.stringify(eventUpdated));
-
-//        },
-
-/*getEvents: function () {
-            return JSON.parse(localStorage.getItem('events'));
-        }*/
-//    }
-//
-//    return 1;
