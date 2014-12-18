3207392d957f2f1b992fa292e48eeee7ded74e4a
Vincentxyz <vincentmzw@gmail.com>
2014-12-18 22:31:48 +0100

    Erste Unit Tests und Gruntfile wieder hinein

--- app/scripts/play.js
+++ app/scripts/play.js
@@ -1,36 +1,21 @@
 angular.module('play', [])
 
-.controller('playCtrl', function($scope, cssInjector) {
+.controller('playCtrl', ['$scope', 'cssInjector',  function($scope, cssInjector) {
 
-  cssInjector.add('styles/play.css');
 
-  $scope.storageTasklines = window.localStorage.getItem("task");
+    
+    //fetch all data from localStorage from tab-home 
+    $scope.fetchDataFromLocalStorage = function (){
+         $scope.task = window.localStorage.getItem("task");
     
 //$scope.teammatePhotos{}
     
- //fetch all data from localStorage from tab-home 
   $scope.name = window.localStorage.getItem("username");
     
   $scope.isEnumeration = window.localStorage.getItem("isEnumeration");
+    }
     
-  $scope.isText = false;
-    
-
-
-
-
-  if ($scope.isEnumeration == false) {
-    $scope.tasklines = $scope.storageTasklines;
-
-  } else if ($scope.isEnumeration) {
-    var index = $scope.storageTasklines.indexOf(';');
-    $scope.tasklineOne = $scope.storageTasklines.slice(0, index);
-    var enumerationLines = $scope.storageTasklines.slice(index + 1, $scope.storageTasklines.length);
-    $scope.enumeration = enumerationLines.split(';');
-  }
-
-  <!-- Code für den Picture Slider -->
-  $scope.slides = [{
+     $scope.slides = [{
       image: 'img/Megan_1.jpg',
     }, {
       image: 'img/Megan_2.jpg',
@@ -39,7 +24,29 @@ angular.module('play', [])
   ];
 
 
-})
+
+//check if task is a enumeration and split it if it is
+
+$scope.checkEnumeration = function (){    
+  if ($scope.isEnumeration == false) {
+    $scope.tasklines = $scope.task;
+
+  } else if ($scope.isEnumeration == true) {
+    var index = $scope.task.indexOf(';');
+    $scope.tasklineOne = $scope.task.slice(0, index);
+    var enumerationLines = $scope.task.slice(index + 1, $scope.task.length);
+    $scope.enumeration = enumerationLines.split(';');
+  }
+
+};
+
+    //executeFunctions
+    $scope.fetchDataFromLocalStorage();
+    $scope.checkEnumeration();
+    
+    
+}])
+
 
 
 ;
