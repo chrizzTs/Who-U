9057329fc82b29d7994b2b2d33996df6575707b5
Vincentxyz <vincentmzw@gmail.com>
2014-12-20 14:57:20 +0100

    Rudimentary Camera Function

--- app/scripts/pictureTaker.js
+++ app/scripts/pictureTaker.js
@@ -1,35 +1,119 @@
+'use strict';
+
 angular.module('pictureTaker', [])
 
+
+.factory('PhoneCamera', ['$q', 
+  function($q) {
     
-.factory('Camera', ['$q', function($q) {
+      
+    return {
+      getPicture: function(options) {
+        var q = $q.defer();
 
-  return {
-    getPicture: function(options) {
-      var q = $q.defer();
-
-      navigator.camera.getPicture(function(result) {
-        // Do any magic you need
-        q.resolve(result);
-      }, function(err) {
-        q.reject(err);
-      }, options);
-
-      return q.promise;
+        navigator.camera.getPicture(function(result) {
+          // Do any magic you need
+          q.resolve(result);
+        }, function(err) {
+          q.reject(err);
+        }, {quality: 50,
+           destinationType: Camera.DestinationType.FILE_URI,
+           sourceType: Camera.PictureSourceType.CAMERA});
+          
+        return q.promise;
+      
     }
   }
-}])
-    
-.controller('CameraCtrl', ['$scope', 'Camera', function($scope, Camera) {
+  }])
 
-  $scope.getPhoto = function() {
-    Camera.getPicture().then(function(imageURI) {
+
+.factory('PhoneLibrary', ['$q', 
+  function($q) {
+    
+      
+    return {
+      getPicture: function(options) {
+        var q = $q.defer();
+
+        navigator.camera.getPicture(function(result) {
+          // Do any magic you need
+          q.resolve(result);
+        }, function(err) {
+          q.reject(err);
+        }, {quality: 50,
+           destinationType: Camera.DestinationType.FILE_URI,
+           sourceType: Camera.PictureSourceType.PHOTOLIBRARY});
+          
+        return q.promise;
+      
+    }
+  }
+  }])
+
+.factory('PhoneAlbum', ['$q', 
+  function($q) {
+    
+      
+    return {
+      getPicture: function(options) {
+        var q = $q.defer();
+
+        navigator.camera.getPicture(function(result) {
+          // Do any magic you need
+          q.resolve(result);
+        }, function(err) {
+          q.reject(err);
+        }, {quality: 50,
+           destinationType: Camera.DestinationType.FILE_URI,
+           sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM});
+          
+        return q.promise;
+      
+    }
+  }
+  }])
+
+
+.controller('CameraCtrl', ['$scope', 'PhoneCamera', 'PhoneLibrary', 'PhoneAlbum', 'cssInjector',
+
+  function($scope, PhoneCamera, PhoneLibrary, PhoneAlbum, cssInjector) {
+
+    //add Styles
+    cssInjector.add('styles/pictureTaker.css');
+
+      $scope.getCameraPhoto = function() {
+    PhoneCamera.getPicture().then(function(imageURI) {
+        $scope.cameraPic = imageURI;
       console.log(imageURI);
     }, function(err) {
       console.err(err);
     });
-  }}])
-    
-.config(function($compileProvider){
+  };
+      
+       $scope.getLibraryPhoto = function() {
+    PhoneLibrary.getPicture().then(function(imageURI) {
+        $scope.cameraPic = imageURI;
+      console.log(imageURI);
+    }, function(err) {
+      console.err(err);
+    });
+  };
+      
+       $scope.getAlbumPhoto = function() {
+    PhoneAlbum.getPicture().then(function(imageURI) {
+        $scope.cameraPic = imageURI;
+      console.log(imageURI);
+    }, function(err) {
+      console.err(err);
+    });
+  };
+      
+   
+
+  }
+])
+
+.config(function($compileProvider) {
   $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
 })
 
--- app/scripts/play.js
+++ app/scripts/play.js
@@ -1,52 +1,56 @@
+'use strict';
+
 angular.module('play', [])
 
-.controller('playCtrl', ['$scope', 'cssInjector',  function($scope, cssInjector) {
+.controller('playCtrl', ['$scope', 'cssInjector',
+  function($scope, cssInjector) {
 
 
-    cssInjector.add("styles/play.css");
-    
-    //fetch all data from localStorage from tab-home 
-    $scope.fetchDataFromLocalStorage = function (){
-         $scope.task = window.localStorage.getItem("task");
-    
-//$scope.teammatePhotos{}
-    
-  $scope.name = window.localStorage.getItem("username");
-    
-  $scope.isEnumeration = window.localStorage.getItem("isEnumeration");
-    }
-    
-     $scope.slides = [{
-      image: 'img/picture_1.jpg',
-    }, {
-      image: 'img/picture_2.jpg',
-    }
+    cssInjector.add('styles/play.css');
 
-  ];
+    //fetch all data from localStorage from tab-home
+    $scope.fetchDataFromLocalStorage = function() {
+      $scope.task = window.localStorage.getItem('task');
+
+      //$scope.teammatePhotos{}
+
+      $scope.name = window.localStorage.getItem('username');
+
+      $scope.isEnumeration = window.localStorage.getItem('isEnumeration');
+    };
+
+    $scope.slides = [{
+        image: 'img/picture_1.jpg',
+      }, {
+        image: 'img/picture_2.jpg',
+      }
+
+    ];
 
 
 
-//check if task is a enumeration and split it if it is
+    //check if task is a enumeration and split it if it is
 
-$scope.checkEnumeration = function (){    
-  if ($scope.isEnumeration == false) {
-    $scope.tasklines = $scope.task;
+    $scope.checkEnumeration = function() {
+      if ($scope.isEnumeration === false) {
+        $scope.tasklines = $scope.task;
 
-  } else if ($scope.isEnumeration == true) {
-    var index = $scope.task.indexOf(';');
-    $scope.tasklineOne = $scope.task.slice(0, index);
-    var enumerationLines = $scope.task.slice(index + 1, $scope.task.length);
-    $scope.enumeration = enumerationLines.split(';');
-  }
+      } else if ($scope.isEnumeration === true) {
+        var index = $scope.task.indexOf(';');
+        $scope.tasklineOne = $scope.task.slice(0, index);
+        var enumerationLines = $scope.task.slice(index + 1, $scope.task.length);
+        $scope.enumeration = enumerationLines.split(';');
+      }
 
-};
+    };
 
     //executeFunctions
     $scope.fetchDataFromLocalStorage();
     $scope.checkEnumeration();
-    
-    
-}])
+
+
+  }
+])
 
 
 
