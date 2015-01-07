22eca723feb0339ae3a703b3c1eda0b0947a28c3
Mats77 <m.wieben@yahoo.de>
2015-01-07 10:43:42 +0100

    getPhoto fixes

--- app/scripts/photos.js
+++ app/scripts/photos.js
@@ -6,45 +6,49 @@
 
 angular.module('photos', [])
 
-.controller('photosCtrl', ['$scope', 'cssInjector', 'serverAPI', function ($scope, cssInjector, serverAPI) {
+.controller('photosCtrl', ['$scope', 'cssInjector', 'serverAPI',
+    function ($scope, cssInjector, serverAPI) {
 
-cssInjector.removeAll();
-    
- $scope.userID = JSON.parse(window.localStorage.getItem('Credentials')).UID;
-    
- cssInjector.add('styles/photos.css');
-    
-  $scope.loaded = false;
-  
-    $scope.photoIds = 
-     JSON.parse(window.localStorage.getItem('photoIds'));
-    
-    console.log($scope.photoIds);
-    
-    
-    $scope.images = new Array();
-    
-    for(var i=0; i < $scope.photoIds.length; i++){
-        serverAPI.getPhoto($scope.userID, $scope.photoIds[i], function(data)
-                           {$scope.images.push(data)});
-    }
-    
-/*  $scope.images = [
+        cssInjector.removeAll();
+
+        $scope.userID = JSON.parse(window.localStorage.getItem('Credentials')).UID;
+
+        cssInjector.add('styles/photos.css');
+
+        $scope.loaded = false;
+
+        $scope.photoIds =
+            JSON.parse(window.localStorage.getItem('photoIds'));
+
+        console.log($scope.photoIds);
+
+
+        $scope.images = new Array();
+
+        for (var i = 0; i < $scope.photoIds.length; i++) {
+            serverAPI.getPhoto($scope.userID, $scope.photoIds[i], function (data) {
+                console.log(data);
+                console.log(data.length)
+                $scope.images.push(data);
+            });
+        }
+
+        /*  $scope.images = [
     'img/picture_1.jpg',
     'img/picture_2.jpg',
     'img/Megan_2.jpg',
       'img/Megan_1.jpg'
   ];*/
-    
-    
-  
-  $scope.selection = $scope.images[0];
-  
-  $scope.setHero = function (img) {
-    $scope.selection = img;
-  }
-  
-  $scope.loaded = true;
 
-                         
+
+
+        $scope.selection = $scope.images[0];
+
+        $scope.setHero = function (img) {
+            $scope.selection = img;
+        }
+
+        $scope.loaded = true;
+
+
                          }]);
