96f21ae911cbc0a53ccfc44119349f7ee5330658
Simon-graiff <s.graiff@outlook.com>
2015-01-11 21:27:12 +0100

    Größere Sterne für eure Riesengriffel. Zusätzlich haben wir jetzt Ionicons lokal, deshalb ist der Push so groß

--- app/scripts/feedback.js
+++ app/scripts/feedback.js
@@ -2,8 +2,11 @@ angular.module('feedback', ['serverAPI'])
 
 .controller('feedbackCtrl', function ($scope, cssInjector, serverAPI) {
 
+    cssInjector.add('styles/feedback.css');
+
     //Counter for counting in array with not yet rated games
     $scope.counter = 0;
+    var stayInTouch = 'true';
 
     $scope.UID = JSON.parse(window.localStorage.getItem('Credentials')).UID;
 
@@ -11,7 +14,6 @@ angular.module('feedback', ['serverAPI'])
     $scope.question1;
     $scope.question2 = 0;
     $scope.question3 = 0;
-    $scope.formData = {};
 
     $scope.openGames = [];
     serverAPI.getGamesToRate($scope.UID, function (data) {
@@ -25,50 +27,61 @@ angular.module('feedback', ['serverAPI'])
         });
     });
 
+    //Doesn't work for now...why?!?
+    if (stayInTouch == 'true') {
+        $scope.userChoiceContact = {
+            mode: true
+        };
+    } else {
+        $scope.userChoiceContact = {
+            mode: false
+        };
+    }
+
     $scope.starsQuestion2 = [
         {
             id: 0,
             action: 'rateQuestion2(1)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }, {
             id: 1,
             action: 'rateQuestion2(2)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }, {
             id: 2,
             action: 'rateQuestion2(3)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }, {
             id: 3,
             action: 'rateQuestion2(4)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }, {
             id: 4,
             action: 'rateQuestion2(5)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }]
 
     $scope.starsQuestion3 = [
         {
             id: 0,
             action: 'rateQuestion3(1)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }, {
             id: 1,
             action: 'rateQuestion3(2)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }, {
             id: 2,
             action: 'rateQuestion3(3)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }, {
             id: 3,
             action: 'rateQuestion3(4)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }, {
             id: 4,
             action: 'rateQuestion3(5)',
-            icon: 'icon ion-android-star'
+            icon: 'ios7-star-outline'
        }]
 
     $scope.contacted = function () {
@@ -79,7 +92,7 @@ angular.module('feedback', ['serverAPI'])
 
     $scope.notContacted = function () {
         serverAPI.insertNewRating($scope.openGames[$scope.counter].otherPlayerId, 0, $scope.openGames[$scope.counter].gameId, function (data) {
-            console.log(data);
+            console.log('Insert new rating:' + data);
         });
 
         console.log("No contact");
@@ -88,7 +101,7 @@ angular.module('feedback', ['serverAPI'])
 
     $scope.rateQuestion2 = function (x) {
         for (var i = 0; i < $scope.starsQuestion2.length; i++) {
-            $scope.starsQuestion2[i].icon = 'ion-android-star';
+            $scope.starsQuestion2[i].icon = 'ios7-star-outline';
         }
 
         $scope.question2 = x;
@@ -97,14 +110,14 @@ angular.module('feedback', ['serverAPI'])
 
         for (var i = 0; i < $scope.starsQuestion2.length; i++) {
             if ($scope.starsQuestion2[i].id <= selected) {
-                $scope.starsQuestion2[i].icon = 'ion-asterisk';
+                $scope.starsQuestion2[i].icon = 'ios7-star';
             }
         }
     }
 
     $scope.rateQuestion3 = function (x) {
         for (var i = 0; i < $scope.starsQuestion3.length; i++) {
-            $scope.starsQuestion3[i].icon = 'ion-android-star';
+            $scope.starsQuestion3[i].icon = 'ios7-star-outline';
         }
 
         $scope.question3 = x;
@@ -113,7 +126,7 @@ angular.module('feedback', ['serverAPI'])
 
         for (var i = 0; i < $scope.starsQuestion3.length; i++) {
             if ($scope.starsQuestion3[i].id <= selected) {
-                $scope.starsQuestion3[i].icon = 'ion-asterisk'
+                $scope.starsQuestion3[i].icon = 'ios7-star'
             }
         }
     }
@@ -124,6 +137,17 @@ angular.module('feedback', ['serverAPI'])
         }
     }
 
+    $scope.changeUserChoiceContact = function () {
+        if (stayInTouch == 'true') {
+            console.log('user hat keinen Bock auf dich');
+            stayInTouch = 'false';
+        } else {
+            console.log('user steht auf dich');
+            stayInTouch = 'true';
+        }
+
+    }
+
     $scope.evaluate = function () {
         var scoreQuestion1 = 5;
         var scoreQuestion2 = parseInt($scope.question2);
@@ -132,7 +156,7 @@ angular.module('feedback', ['serverAPI'])
         console.log(finalScore);
 
         serverAPI.insertNewRating($scope.openGames[$scope.counter].otherPlayerId, finalScore, $scope.openGames[$scope.counter].gameId, function (data) {
-            console.log(data);
+            console.log('Insert new rating: ' + data);
 
             $scope.counter++;
             if ($scope.counter < $scope.openGames.length) {
