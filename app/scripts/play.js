1973d1db83fab793f3d3debe161e5a5d50945930
Vincentxyz <vincentmzw@gmail.com>
2015-01-13 14:02:11 +0100

    Play Screen Correction

--- app/scripts/play.js
+++ app/scripts/play.js
@@ -3,47 +3,56 @@
 angular.module('play', [])
 
 .controller('playCtrl', ['$scope', 'cssInjector', 'serverAPI',
-  function($scope, cssInjector, serverAPI) {
+    function($scope, cssInjector, serverAPI) {
 
 
-    cssInjector.add('styles/play.css');
+      cssInjector.add('styles/play.css');
 
       $scope.skipUser = false;
-      
-     // $scope.skipUser = window.localStorage.getItem('skipUser');
-      
+
+      // $scope.skipUser = window.localStorage.getItem('skipUser');
+
       $scope.task;
-    //fetch all data from localStorage from tab-home
-    $scope.fetchDataFromLocalStorage = function() {
+      //fetch all data from localStorage from tab-home
+      $scope.fetchDataFromLocalStorage = function() {
 
-      //$scope.teammatePhotos{}
+        //$scope.teammatePhotos{}
 
-      $scope.name = window.localStorage.getItem('teammate');
+        $scope.name = window.localStorage.getItem('teammate');
 
-      $scope.isEnumeration = window.localStorage.getItem('isEnumeration');
-      
+        $scope.isEnumeration = window.localStorage.getItem('isEnumeration');
+
         $scope.task = window.localStorage.getItem('task');
-    };
-      
-      $scope.slides = new Array();
-    
-      serverAPI.getPhoto(window.localStorage.getItem('teammateUID'), $scope.photoIds[i], function(data) {
-            
-        $scope.imageJson = data;
-            
-        //array of JSONs with the photoId and data is pushed into localStorage
-        var entry  = {
-            "photoId" : $scope.imageJson.id,
-            "image" : $scope.imageJson.data
-        };
-             
-          $scope.slides.push(entry);
-          if (i == $scope.photoIds.length) {
-            window.localStorage.setItem('userPhotos', JSON.stringify($scope.slides));
-          }
-      });
+      };
 
-/*
+      $scope.slides = new Array();
+
+      serverAPI.getUserData(window.localStorage.getItem('teammateUID'), function(data) {
+          $scope.photoIds = window.localStorage.getItem('photoIds');
+
+          $scope.photoIds = data.photoIds;
+
+          //loop for getting the image data of every photo
+          for (var i = 0; i < $scope.photoIds.length; i++) {
+
+            serverAPI.getPhoto(window.localStorage.getItem('teammateUID'), $scope.photoIds[i], function(data) {
+
+              $scope.imageJson = data;
+
+              //array of JSONs with the photoId and data is pushed into localStorage
+              var entry = {
+                "photoId": $scope.imageJson.id,
+                "image": $scope.imageJson.data
+              };
+
+              $scope.slides.push(entry);
+              if (i == $scope.photoIds.length) {
+                window.localStorage.setItem('userPhotos', JSON.stringify($scope.slides));
+              }
+            })
+          }
+        })
+        /*
 
     $scope.slides = [{
         image: 'img/picture_1.jpg',
@@ -55,27 +64,26 @@ angular.module('play', [])
 */
 
 
-    //check if task is a enumeration and split it if is
+        //check if task is a enumeration and split it if is
 
-    $scope.checkEnumeration = function() {
-      if ($scope.isEnumeration == 0) {
-        $scope.tasklines = $scope.task;
+        $scope.checkEnumeration = function() {
+          if ($scope.isEnumeration == 0) {
+            $scope.tasklines = $scope.task;
 
-      } else if ($scope.isEnumeration == 1) {
-        var index = $scope.task.indexOf(';');
-        $scope.tasklineOne = $scope.task.slice(0, index);
-        var enumerationLines = $scope.task.slice(index + 1, $scope.task.length);
-        $scope.enumeration = enumerationLines.split(';');
+          } else if ($scope.isEnumeration == 1) {
+            var index = $scope.task.indexOf(';');
+            $scope.tasklineOne = $scope.task.slice(0, index);
+            var enumerationLines = $scope.task.slice(index + 1, $scope.task.length);
+            $scope.enumeration = enumerationLines.split(';');
+          }
+
+        };
+
+        //executeFunctions
+        $scope.fetchDataFromLocalStorage(); $scope.checkEnumeration();
+
+
       }
+      ])
 
-    };
-
-    //executeFunctions
-    $scope.fetchDataFromLocalStorage();
-    $scope.checkEnumeration();
-
-
-  }
-])
-
-;
+  ;
