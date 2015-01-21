15081f6858f1f23a87f9fbe0d5e027d1d9f8d325
Vincentxyz <vincentmzw@gmail.com>
2015-01-22 00:08:28 +0100

    Icons set and doneLoading

--- app/scripts/photos.js
+++ app/scripts/photos.js
@@ -23,6 +23,7 @@ angular.module('photos', [])
 
     //initialize the selection photo and the selected photoId
     $scope.selection;
+    $scope.doneLoading = false;
     $scope.selectionPhotoId = 0;
     $scope.profilePhotoId = 0;
     //initialize the array in which images of the user will be stored
@@ -126,6 +127,7 @@ angular.module('photos', [])
               $scope.setHero($scope.images[0]);
             if (i == $scope.photoIds.length) {
               window.localStorage.setItem('userPhotos', JSON.stringify($scope.images));
+                $scope.doneLoading = true;
             }
             console.log('Loaded from Server:' + entry.photoId);
 
